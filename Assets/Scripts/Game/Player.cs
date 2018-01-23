04d81ca60482b45bf7b73f0c5c89bfb7d9c50486
in3orn <in3orn@gmail.com>
2018-01-23 22:36:03 +0100

    Basic player animation

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -27,16 +27,32 @@ public class Player : MonoBehaviour
 
     private CircleCollider2D circleCollider;
 
+    [SerializeField]
     private Showable showable;
 
     private float showInterval;
 
     private float hideInterval;
 
+    [SerializeField]
+    private Animator playerAnimator;
+
+    private Vector3 target;
+
+    private float velocity;
+
+    [SerializeField]
+    private float maxVelocity;
+
+    [SerializeField]
+    private float acceleration;
+
+    [SerializeField]
+    private float errorMargin;
+
     void Awake()
     {
         circleCollider = GetComponent<CircleCollider2D>();
-        showable = GetComponent<Showable>();
 
         moves = new Queue<Vector2>(2);
     }
@@ -45,6 +61,7 @@ public class Player : MonoBehaviour
     {
         state = StateEnum.Idle;
 
+        target = position;
         transform.position = position;
         transform.localScale = Vector3.one;
 
@@ -62,19 +79,83 @@ public class Player : MonoBehaviour
         return state != StateEnum.Moving;
     }
 
-    public void Move(Vector2 vector)
+    void Update()
+    {
+        Vector3 diff = target - transform.position;
+        float distance = diff.magnitude;
+
+        if (distance > errorMargin)
+        {
+            state = StateEnum.Moving;
+            velocity += acceleration;
+            velocity = Mathf.Min(velocity, maxVelocity);
+
+            if (distance > velocity)
+            {
+                transform.position += diff.normalized * velocity;
+            }
+            else
+            {
+                transform.position = target;
+                state = StateEnum.Idle;
+                if (OnMoved != null)
+                    OnMoved();
+            }
+        }
+        else
+        {
+            velocity = 0f;
+        }
+
+        playerAnimator.SetFloat("velocity", velocity);
+    }
+
+    public void Move(Vector3 vector)
     {
         if (state != StateEnum.Moving)
         {
-            Vector2 target = (Vector2)transform.position + vector;
-            StartCoroutine(move(transform.position, target));
+            target = transform.position + vector;
+            ChangeDirection(vector);
+            //StartCoroutine(Move(transform.position, target));
 
             showable.ShowDelay = (target.x / Field.SIZE + target.y / Field.SIZE) * showInterval;
             showable.HideDelay = (target.x / Field.SIZE + target.y / Field.SIZE) * hideInterval;
         }
     }
 
-    private IEnumerator move(Vector2 start, Vector2 end)
+    private void ChangeDirection(Vector2 vector)
+    {
+        float angle = Mathf.Atan2(vector.y, vector.x);
+
+        while (angle < 0)
+        {
+            angle += Mathf.PI * 2f;
+        }
+
+        while (angle > Mathf.PI * 2f)
+        {
+            angle -= Mathf.PI * 2f;
+        }
+
+        if (Mathf.Abs(angle) < 0.1f)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (Mathf.Abs(angle - Mathf.PI * 0.5f) < 0.1f)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        else if (Mathf.Abs(angle - Mathf.PI) < 0.1f)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        else if (Mathf.Abs(angle - Mathf.PI * 1.5f) < 0.1f)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 270);
+        }
+    }
+
+    private IEnumerator Move(Vector2 start, Vector2 end)
     {
         state = StateEnum.Moving;
 
@@ -86,8 +167,15 @@ public class Player : MonoBehaviour
 
             if (t >= 1) break;
             yield return null;
+
+            playerAnimator.SetFloat("velocity", 1.0f);
         }
 
+        //TODO change to update routine - better for velocity checking, etc.
+        // + queue, then can automatically extend move to another field
+        // + size in (0, 1) (relative to Field.SIZE)
+        // - velocity cannnot be relative if acceleration is used
+        playerAnimator.SetFloat("velocity", 0f);
         state = StateEnum.Idle;
         OnMoved();
     }
