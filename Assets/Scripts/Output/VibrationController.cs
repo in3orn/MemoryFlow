ea0b625991c51a2f86f0ae10b4140d74ff4aa0d3
in3orn <in3orn@gmail.com>
2018-02-03 10:26:43 +0100

    Vibration after failure

---
+++ Assets/Scripts/Output/VibrationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Dev.Krk.MemoryFlow.Game;
+using UnityEngine;
+
+public class VibrationController : MonoBehaviour
+{
+    [SerializeField]
+    private LevelController levelController;
+
+    void Start()
+    {
+
+    }
+
+    void OnEnable()
+    {
+        levelController.OnPlayerFailed += ProcessPlayerFailed;
+    }
+
+    void OnDisable()
+    {
+        if(levelController != null)
+        {
+            levelController.OnPlayerFailed -= ProcessPlayerFailed;
+        }
+    }
+
+    private void ProcessPlayerFailed()
+    {
+        Handheld.Vibrate();
+    }
+}
